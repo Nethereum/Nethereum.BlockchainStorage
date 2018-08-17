25272f1d1f756db4c6f734f3f5300bfe2f295969
Dave Whiffin <dave.whiffin@gmail.com>
2018-08-17 13:37:05 +0100

    Fixing context initialisation for EF Core Sqlite

--- Nethereum.BlockchainStore.EFCore.Tests/Sqlite/TestSqliteDbContextFactory.cs
+++ Nethereum.BlockchainStore.EFCore.Tests/Sqlite/TestSqliteDbContextFactory.cs
@@ -1,14 +1,29 @@
-﻿using Nethereum.BlockchainStore.EFCore.Sqlite;
+﻿using Microsoft.EntityFrameworkCore;
+using Nethereum.BlockchainStore.EFCore.Sqlite;
 
 namespace Nethereum.BlockchainStore.EFCore.Tests.Sqlite
 {
     public class TestSqliteDbContextFactory : BlockchainDbContextFactory
     {
+        public static readonly object _lock = new object();
+        public static bool _dbExists = false;
+
         public const string ConnectionString =
-            "Data Source=C:/dev/repos/Nethereum.BlockchainStorage/Nethereum.BlockchainStore.EFCore.Sqlite/Blockchain.db";
+            "Data Source=C:/temp/Blockchain_EFCore_UnitTest.db";
 
         public TestSqliteDbContextFactory() : base(ConnectionString)
         {
+            if (!_dbExists)
+            {
+                lock (_lock)
+                {
+                    if (!_dbExists)
+                    {
+                        new BlockchainDbContext(ConnectionString).Database.EnsureCreated();
+                        _dbExists = true;
+                    }
+                }
+            }
         }
     }
 }
