799fb71b9a59984561a24024775754fdf0254114
Dave Whiffin <dave.whiffin@gmail.com>
2018-08-17 13:52:50 +0100

    Small azure storage console refactor

--- Nethereum.BlockchainStore.AzureTables.Console/Program.cs
+++ Nethereum.BlockchainStore.AzureTables.Console/Program.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Diagnostics;
-using Nethereum.BlockchainStore.Bootstrap;
+﻿using Nethereum.BlockchainStore.Bootstrap;
 
 namespace Nethereum.BlockchainStore.Processing.Console
 {
     public class Program
     {
-        private static readonly string prefix = "Rinkeby";
         private static readonly string connectionString = "<put your connection string here>";
 
         public static int Main(string[] args)
         {
-            var repositoryFactory = new CloudTableSetup(connectionString, prefix);
             var presetName = args?.Length == 0 ? ProcessorConfigurationPresets.Default : args[0];
             var configuration = ProcessorConfigurationPresets.Get(presetName);
+            var repositoryFactory = new CloudTableSetup(connectionString, configuration.Schema);
             return ProcessorConsole.Execute(args, repositoryFactory, configuration).Result;
         }
     }
