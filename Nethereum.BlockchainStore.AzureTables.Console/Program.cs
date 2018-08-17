c628bc5b97b1d68b4a590922f21bc29897a71cc8
Dave Whiffin <dave.whiffin@gmail.com>
2018-08-17 13:50:38 +0100

    config change

--- Nethereum.BlockchainStore.AzureTables.Console/Program.cs
+++ Nethereum.BlockchainStore.AzureTables.Console/Program.cs
@@ -7,7 +7,7 @@ namespace Nethereum.BlockchainStore.Processing.Console
     public class Program
     {
         private static readonly string prefix = "Rinkeby";
-        private static readonly string connectionString = "DefaultEndpointsProtocol=https;AccountName=davewhiffin;AccountKey=uy2NcHMeK2emMTt5iFcBm/SvkQYi8IEfXKF+L6kGOxEffjRvlUUUYxXPECD+rVIRW4FbEB6MF9/jSuBJaUve7Q==;EndpointSuffix=core.windows.net";
+        private static readonly string connectionString = "<put your connection string here>";
 
         public static int Main(string[] args)
         {
