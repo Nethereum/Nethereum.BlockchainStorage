5d52fa911c2bb09aa11b6436dc76e67f29c1fa39
Dave Whiffin <dave.whiffin@gmail.com>
2018-10-11 14:18:05 +0100

    WaitStrategy tidy

--- Nethereum.BlockchainStore/Processing/WaitStrategy.cs
+++ Nethereum.BlockchainStore/Processing/WaitStrategy.cs
@@ -5,13 +5,13 @@ namespace Nethereum.BlockchainStore.Processing
 {
     public class WaitStrategy : IWaitStrategy
     {
-        private readonly int[] _waitIntervals = {1000, 2000, 5000, 10000, 15000};
+        private static readonly int[] WaitIntervals = {1000, 2000, 5000, 10000, 15000};
 
         public async Task Apply(int retryCount)
         {
-            var intervalMs = retryCount >= _waitIntervals.Length ? 
-                _waitIntervals.Last() : 
-                _waitIntervals[retryCount];
+            var intervalMs = retryCount >= WaitIntervals.Length ? 
+                WaitIntervals.Last() : 
+                WaitIntervals[retryCount];
 
             await Task.Delay(intervalMs);
         }
