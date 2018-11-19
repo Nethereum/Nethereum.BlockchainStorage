667f8933b40b82abde7addc34801aead9bb388a2
Dave Whiffin <dave.whiffin@gmail.com>
2018-11-19 09:52:25 +0000

    TransactionWithReceiptTests

--- Nethereum.BlockchainProcessing/Handlers/ContractCreationTransaction.cs
+++ Nethereum.BlockchainProcessing/Handlers/ContractCreationTransaction.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using Nethereum.BlockchainProcessing.Processors;
-using Nethereum.Contracts;
+﻿using Nethereum.Contracts;
 using Nethereum.Contracts.Extensions;
 using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.DTOs;
 
 namespace Nethereum.BlockchainProcessing.Handlers
 {
-    public class ContractCreationTransaction
+    public class ContractCreationTransaction: TransactionWithReceipt
     {
         public ContractCreationTransaction()
         {
                 
         }
 
-        public ContractCreationTransaction(string contractAddress, string code, Transaction transaction, TransactionReceipt transactionReceipt, bool failedCreatingContract, HexBigInteger blockTimestamp)
+        public ContractCreationTransaction(
+            string contractAddress, 
+            string code, 
+            Transaction transaction, 
+            TransactionReceipt transactionReceipt, 
+            bool failedCreatingContract, 
+            HexBigInteger blockTimestamp)
         {
             ContractAddress = contractAddress;
             Code = code;
@@ -26,15 +30,7 @@ namespace Nethereum.BlockchainProcessing.Handlers
 
         public string ContractAddress { get; private set; }
         public string Code { get; private set; }
-        public Transaction Transaction { get; private set; }
-        public TransactionReceipt TransactionReceipt { get; private set; }
         public bool FailedCreatingContract { get; private set; }
-        public HexBigInteger BlockTimestamp { get; private set; }
-
-        public virtual string[] GetAllRelatedAddresses()
-        {
-            return Transaction?.GetAllRelatedAddresses(TransactionReceipt) ?? Array.Empty<string>();
-        }
 
         public virtual TDeploymentMessage DecodeToDeploymentMessage<TDeploymentMessage>()
             where TDeploymentMessage : ContractDeploymentMessage, new()
--- Nethereum.BlockchainProcessing/Handlers/TransactionWithReceipt.cs
+++ Nethereum.BlockchainProcessing/Handlers/TransactionWithReceipt.cs
@@ -1,4 +1,6 @@
-﻿using Nethereum.BlockchainProcessing.Processors;
+﻿using System;
+using System.Numerics;
+using Nethereum.BlockchainProcessing.Processors;
 using Nethereum.Contracts;
 using Nethereum.Contracts.Extensions;
 using Nethereum.Hex.HexTypes;
@@ -8,12 +10,20 @@ namespace Nethereum.BlockchainProcessing.Handlers
 {
     public class TransactionWithReceipt
     {
+        static readonly HexBigInteger UndefinedBlockNumber = new HexBigInteger(BigInteger.MinusOne);
+        
         public TransactionWithReceipt()
         {
             
         }
 
-        public TransactionWithReceipt(Transaction transaction, TransactionReceipt transactionReceipt, bool hasError, HexBigInteger blockTimestamp, string error = null, bool hasVmStack = false)
+        public TransactionWithReceipt(
+            Transaction transaction, 
+            TransactionReceipt transactionReceipt, 
+            bool hasError, 
+            HexBigInteger blockTimestamp, 
+            string error = null, 
+            bool hasVmStack = false)
         {
             Transaction = transaction;
             TransactionReceipt = transactionReceipt;
@@ -23,16 +33,21 @@ namespace Nethereum.BlockchainProcessing.Handlers
             HasVmStack = hasVmStack;
         }
 
-        public Transaction Transaction { get; private set; }
-        public TransactionReceipt TransactionReceipt { get; private set; }
-        public bool HasError { get; private set; }
-        public HexBigInteger BlockTimestamp { get; private set; }
-        public string Error { get; private set; }
-        public bool HasVmStack { get; private set; }
+        public Transaction Transaction { get; protected set; }
+        public TransactionReceipt TransactionReceipt { get; protected set; }
+        public bool HasError { get; protected set; }
+        public HexBigInteger BlockTimestamp { get; protected set; }
+        public string Error { get; protected set; }
+        public bool HasVmStack { get; protected set; }
+
+        public virtual HexBigInteger BlockNumber => Transaction?.BlockNumber ?? UndefinedBlockNumber;
+        public virtual string TransactionHash => Transaction?.TransactionHash;
+        public virtual bool Succeeded => TransactionReceipt?.Succeeded() ?? false;
+        public virtual bool Failed => !Succeeded;
 
         public virtual string[] GetAllRelatedAddresses()
         {
-            return Transaction?.GetAllRelatedAddresses(TransactionReceipt);
+            return Transaction?.GetAllRelatedAddresses(TransactionReceipt) ?? Array.Empty<string>();
         }
 
         public virtual bool HasLogs()
