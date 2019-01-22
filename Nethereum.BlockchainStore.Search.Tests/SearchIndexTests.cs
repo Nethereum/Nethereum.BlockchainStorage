8adb0ccbc4568df6181c95f52bb90290bd46de2c
Dave Whiffin <dave.whiffin@gmail.com>
2019-01-22 17:50:19 +0000

    Unit tests for SearchIndex

---
+++ Nethereum.BlockchainStore.Search.Tests/SearchIndexTests.cs
@@ -0,0 +1,282 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.BlockchainStore.Search.PurchaseOrderPOC;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using System.Numerics;
+using Xunit;
+
+namespace Nethereum.BlockchainStore.Search.Tests
+{
+    public class SearchIndexTests
+    {
+        public partial class DepositedEventDTO : DepositedEventDTOBase { }
+
+        [Event("Deposited")]
+        public class DepositedEventDTOBase : IEventDTO
+        {
+            [Parameter("address", "_sender", 1, true )]
+            public virtual string Sender { get; set; }
+
+            [Parameter("uint256", "_value", 2, false )]
+            public virtual BigInteger Value { get; set; }
+
+            [Parameter("uint256", "_newBalance", 3, false)]
+            public virtual BigInteger NewBalance { get; set; }
+        }
+
+        [Fact]
+        public void BuildsExpectedIndexForCodeGeneratedEventDto()
+        {
+            var searchIndex = new SearchIndex<DepositedEventDTO>();
+
+            var depositedEventDto = new DepositedEventDTO
+            {
+                NewBalance = new HexBigInteger("100"),
+                Value = new HexBigInteger("10"),
+                Sender = "adsfadsfasdfasdf"
+            };
+
+            Assert.Equal("Deposited", searchIndex.IndexName);
+            Assert.Equal(12, searchIndex.Fields.Length);
+
+            var senderField = searchIndex.Field("Sender");
+            Assert.NotNull(senderField);
+            Assert.Equal(typeof(string), senderField.DataType);
+            Assert.False(senderField.IsKey);
+            Assert.False(senderField.Ignore);
+            Assert.True(senderField.IsFacetable);
+            Assert.True(senderField.IsFilterable);
+            Assert.True(senderField.IsSearchable);
+            Assert.True(senderField.IsSortable);
+            Assert.Equal(depositedEventDto.Sender, senderField.EventValue(depositedEventDto));
+
+            var valueField = searchIndex.Field("Value");
+            Assert.NotNull(valueField);
+            Assert.Equal(typeof(BigInteger), valueField.DataType);
+            Assert.False(valueField.IsKey);
+            Assert.False(valueField.Ignore);
+            Assert.False(valueField.IsSearchable);
+            Assert.False(valueField.IsFilterable);
+            Assert.False(valueField.IsFacetable);
+            Assert.False(valueField.IsSortable);
+            Assert.Equal(depositedEventDto.Value, valueField.EventValue(depositedEventDto));
+
+            var newBalanceField = searchIndex.Field("NewBalance");
+            Assert.NotNull(newBalanceField);
+            Assert.Equal(typeof(BigInteger), newBalanceField.DataType);
+            Assert.False(newBalanceField.IsKey);
+            Assert.False(newBalanceField.Ignore);
+            Assert.False(newBalanceField.IsSearchable);
+            Assert.False(newBalanceField.IsFilterable);
+            Assert.False(newBalanceField.IsFacetable);
+            Assert.False(newBalanceField.IsSortable);
+            Assert.Equal(depositedEventDto.NewBalance, newBalanceField.EventValue(depositedEventDto));
+
+            var log_key = searchIndex.Field("log_key");
+            var log_removed = searchIndex.Field("log_removed");
+            var log_type = searchIndex.Field("log_type");
+            var log_logIndex = searchIndex.Field("log_logIndex");
+            var log_transactionIndex = searchIndex.Field("log_transactionIndex");
+            var log_transactionHash = searchIndex.Field("log_transactionHash");
+            var log_blockHash = searchIndex.Field("log_blockHash");
+            var log_blockNumber = searchIndex.Field("log_blockNumber");
+            var log_address = searchIndex.Field("log_address");
+
+            var filterLog = new FilterLog
+            {
+                Type = "type",
+                Address = "address",
+                BlockHash = "block_hash",
+                BlockNumber = new HexBigInteger(101),
+                TransactionHash = "transaction_hash",
+                TransactionIndex = new HexBigInteger(3),
+                LogIndex = new HexBigInteger(9),
+                Removed = false
+            };
+
+            Assert.NotNull(log_key);
+            Assert.Equal(typeof(string), log_key.DataType);
+            Assert.True(log_key.IsKey);
+            Assert.True(log_key.IsSortable);
+            Assert.False(log_key.Ignore);
+            Assert.False( log_key.IsSearchable);
+            Assert.False(log_key.IsFacetable);
+            Assert.False(log_key.IsFilterable);
+            Assert.Equal("101_3_9", log_key.LogValue(filterLog));
+
+            Assert.NotNull(log_removed);
+            Assert.Equal(typeof(bool), log_removed.DataType);
+            Assert.False(log_removed.Ignore);
+            Assert.False(log_removed.IsKey);
+            Assert.False(log_removed.IsFacetable);
+            Assert.True(log_removed.IsFilterable);
+            Assert.False(log_removed.IsSearchable);
+            Assert.True(log_removed.IsSortable);
+            Assert.Equal(filterLog.Removed, log_removed.LogValue(filterLog));
+            
+            Assert.NotNull(log_type);
+            Assert.Equal(typeof(string), log_type.DataType);
+            Assert.False(log_type.Ignore);
+            Assert.False(log_type.IsKey);
+            Assert.False(log_type.IsFacetable);
+            Assert.True(log_type.IsFilterable);
+            Assert.False(log_type.IsSearchable);
+            Assert.True(log_type.IsSortable);
+            Assert.Equal(filterLog.Type, log_type.LogValue(filterLog));
+
+            Assert.NotNull(log_logIndex);
+            Assert.Equal(typeof(HexBigInteger), log_logIndex.DataType);
+            Assert.False(log_logIndex.Ignore);
+            Assert.False(log_logIndex.IsKey);
+            Assert.False(log_logIndex.IsFacetable);
+            Assert.False(log_logIndex.IsFilterable);
+            Assert.False(log_logIndex.IsSearchable);
+            Assert.False(log_logIndex.IsSortable);
+            Assert.Equal(filterLog.LogIndex, log_logIndex.LogValue(filterLog));
+
+            Assert.NotNull(log_transactionHash);
+            Assert.Equal(typeof(string), log_transactionHash.DataType);
+            Assert.False(log_transactionHash.Ignore);
+            Assert.False(log_transactionHash.IsKey);
+            Assert.False(log_transactionHash.IsFacetable);
+            Assert.False(log_transactionHash.IsFilterable);
+            Assert.True(log_transactionHash.IsSearchable);
+            Assert.False(log_transactionHash.IsSortable);
+            Assert.Equal(filterLog.TransactionHash, log_transactionHash.LogValue(filterLog));
+
+            Assert.NotNull(log_transactionIndex);
+            Assert.Equal(typeof(HexBigInteger), log_transactionIndex.DataType);
+            Assert.False(log_transactionIndex.Ignore);
+            Assert.False(log_transactionIndex.IsKey);
+            Assert.False(log_transactionIndex.IsFacetable);
+            Assert.False(log_transactionIndex.IsFilterable);
+            Assert.False(log_transactionIndex.IsSearchable);
+            Assert.False(log_transactionIndex.IsSortable);
+            Assert.Equal(filterLog.TransactionIndex, log_transactionIndex.LogValue(filterLog));
+
+            Assert.NotNull(log_blockHash);
+            Assert.Equal(typeof(string), log_blockHash.DataType);
+            Assert.False(log_blockHash.Ignore);
+            Assert.False(log_blockHash.IsKey);
+            Assert.False(log_blockHash.IsFacetable);
+            Assert.False(log_blockHash.IsFilterable);
+            Assert.False(log_blockHash.IsSearchable);
+            Assert.False(log_blockHash.IsSortable);
+            Assert.Equal(filterLog.BlockHash, log_blockHash.LogValue(filterLog));
+
+            Assert.NotNull(log_blockNumber);
+            Assert.Equal(typeof(HexBigInteger), log_blockNumber.DataType);
+            Assert.False(log_blockNumber.Ignore);
+            Assert.False(log_blockNumber.IsKey);
+            Assert.False(log_blockNumber.IsFacetable);
+            Assert.True(log_blockNumber.IsFilterable);
+            Assert.True(log_blockNumber.IsSearchable);
+            Assert.True(log_blockNumber.IsSortable);
+            Assert.Equal(filterLog.BlockNumber, log_blockNumber.LogValue(filterLog));
+
+            Assert.NotNull(log_address);
+            Assert.Equal(typeof(string), log_address.DataType);
+            Assert.False(log_address.Ignore);
+            Assert.False(log_address.IsKey);
+            Assert.False(log_address.IsFacetable);
+            Assert.True(log_address.IsFilterable);
+            Assert.True(log_address.IsSearchable);
+            Assert.True(log_address.IsSortable);
+            Assert.Equal(filterLog.Address, log_address.LogValue(filterLog)); 
+        }
+
+        [Searchable(Name = "IndexA")]
+        public partial class CustomEventDtoA : DepositedEventDTOBase
+        {
+            [SearchField("Category", 
+                IsFacetable = true, IsSearchable = true, IsFilterable = true, IsSortable = true)]
+            public virtual string Category { get;set; }
+        }
+
+        [Fact]
+        public void IndexCanIncludeCustomFieldsOutsideOfTheSolidityEvent()
+        {
+            var searchIndex = new SearchIndex<CustomEventDtoA>();
+
+            var categoryField = searchIndex.Field("Category");
+            Assert.NotNull(categoryField);
+            Assert.False(categoryField.Ignore);
+            Assert.False(categoryField.IsKey);
+            Assert.True(categoryField.IsFacetable);
+            Assert.True(categoryField.IsSearchable);
+            Assert.True(categoryField.IsFilterable);
+            Assert.True(categoryField.IsSortable);
+
+            var customEventDto = new CustomEventDtoA() {Category = "CatA"};
+
+            Assert.Equal(customEventDto.Category, categoryField.EventValue(customEventDto));
+
+        }
+
+        [Fact]
+        public void IndexNameCanBeOverridden()
+        {
+            var searchIndex = new SearchIndex<CustomEventDtoA>();
+            Assert.Equal("IndexA", searchIndex.IndexName);
+        }
+
+       
+        public partial class CustomEventDtoB : DepositedEventDTOBase
+        {
+            [SearchField("SenderAddress", 
+                IsFacetable = false, IsSearchable = true, IsFilterable = false, IsSortable = true)]
+            [Parameter("address", "_sender", 1, true )]
+            public override string Sender { get; set; }
+        }
+
+        [Fact]
+        public void EventParametersCanBeCustomIndexed()
+        {
+            var searchIndex = new SearchIndex<CustomEventDtoB>();
+
+            Assert.Null(searchIndex.Field("Sender"));
+
+            var senderAddressField = searchIndex.Field("SenderAddress");
+            Assert.NotNull(senderAddressField);
+            Assert.False(senderAddressField.Ignore);
+            Assert.False(senderAddressField.IsKey);
+            Assert.False(senderAddressField.IsFacetable);
+            Assert.True(senderAddressField.IsSearchable);
+            Assert.False(senderAddressField.IsFilterable);
+            Assert.True(senderAddressField.IsSortable);
+
+            var customEventDto = new CustomEventDtoB() {Sender = "Charles"};
+
+            Assert.Equal(customEventDto.Sender, senderAddressField.EventValue(customEventDto));
+
+        }
+
+        public partial class CustomEventDtoC : DepositedEventDTOBase
+        {
+            [SearchField(Ignore = true)]
+            [Parameter("address", "_sender", 1, true )]
+            public override string Sender { get; set; }
+        }
+
+        [Fact]
+        public void WillExcludeIgnoredFields()
+        {
+            var searchIndex = new SearchIndex<CustomEventDtoC>();
+            Assert.Null(searchIndex.Field("Sender"));
+        }
+
+        public partial class CustomEventDtoD : DepositedEventDTOBase
+        {
+            [SearchField]
+            [Parameter("address", "_sender", 1, true )]
+            public override string Sender { get; set; }
+        }
+
+        [Fact]
+        public void WillProvideDefaultPropertyName()
+        {
+            var searchIndex = new SearchIndex<CustomEventDtoD>();
+            Assert.NotNull(searchIndex.Field("Sender"));
+        }
+    }
+}
---
+++ Nethereum.BlockchainStore.Search/PurchaseOrderPOC/SearchIndex.cs
@@ -0,0 +1,290 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.BlockchainProcessing.Processing.Logs;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Nethereum.BlockchainStore.Search.PurchaseOrderPOC
+{
+    public class SearchIndex
+    {
+        protected Type _eventType;
+        protected EventAttribute _eventAttribute;
+
+        public SearchField[] Fields { get; set; } = Array.Empty<SearchField>();
+        public string IndexName { get; set; }
+    }
+
+    public class SearchIndex<TEvent> : SearchIndex where TEvent : IEventDTO, new()
+    {
+        public SearchIndex()
+        {
+            _eventType = typeof(TEvent);
+            _eventAttribute = _eventType.GetCustomAttribute<EventAttribute>();
+            if (_eventAttribute == null)
+            {
+                throw new ArgumentException("Event class does not have an EventAttribute");
+            }
+
+            //get SearchIndex attribute
+            Searchable searchable = _eventType.GetCustomAttribute<Searchable>();
+            IndexName = searchable == null ? _eventAttribute.Name : searchable.Name;
+
+            var fieldDictionary = new Dictionary<string, SearchField>();
+
+            LoadBlockchainLogFields(fieldDictionary);
+            LoadIndexedFields(fieldDictionary);
+            LoadNonIndexedFields(fieldDictionary);
+            LoadCustomSearchFields(fieldDictionary);
+
+            Fields = fieldDictionary.Values.ToArray();
+        }
+
+        private void LoadBlockchainLogFields(Dictionary<string, SearchField> fieldDictionary)
+        {
+            fieldDictionary.Add("log_key", new SearchField("log_key")
+            {
+                DataType = typeof(string),
+                IsKey = true,
+                IsSortable = true,
+                LogValue = (filter) => 
+                    $"{filter.BlockNumber.Value}_{filter.TransactionIndex.Value}_{filter.LogIndex.Value}"
+            });
+
+            fieldDictionary.Add("log_removed", new SearchField("log_removed")
+            {
+                DataType = typeof(bool),
+                IsSortable = true,
+                IsFilterable = true,
+                LogValue = (filter) => filter.Removed
+            });
+
+            fieldDictionary.Add("log_type", new SearchField("log_type")
+            {
+                DataType = typeof(string),
+                IsSortable = true,
+                IsFilterable = true,
+                LogValue = (filter) => filter.Type
+            });
+
+            fieldDictionary.Add("log_logIndex", new SearchField("log_logIndex")
+            {
+                DataType = typeof(HexBigInteger),
+                IsFilterable = false,
+                LogValue = (filter) => filter.LogIndex
+            });
+            
+            fieldDictionary.Add("log_transactionHash", new SearchField("log_transactionHash")
+            {
+                DataType = typeof(string),
+                IsSearchable = true,
+                LogValue = (filter) => filter.TransactionHash
+            });
+            fieldDictionary.Add("log_transactionIndex", new SearchField("log_transactionIndex")
+            {
+                DataType = typeof(HexBigInteger),
+                LogValue = (filter) => filter.TransactionIndex
+            });
+            fieldDictionary.Add("log_blockHash", new SearchField("log_blockHash")
+            {
+                DataType = typeof(string),
+                LogValue = (filter) => filter.BlockHash
+            });
+            fieldDictionary.Add("log_blockNumber", new SearchField("log_blockNumber")
+            {
+                DataType = typeof(HexBigInteger),
+                IsSearchable = true,
+                IsSortable = true,
+                IsFilterable = true,
+                LogValue = (filter) => filter.BlockNumber
+            });
+            fieldDictionary.Add("log_address", new SearchField("log_address")
+            {
+                DataType = typeof(string),
+                IsSearchable = true,
+                IsSortable = true,
+                IsFilterable = true,
+                LogValue = (filter) => filter.Address
+            });
+
+        }
+
+        private void LoadNonIndexedFields(Dictionary<string, SearchField> fieldDictionary)
+        {
+            var nonIndexedProperties = PropertiesExtractor
+                .GetPropertiesWithParameterAttribute(_eventType)
+                .Select(p => new { property = p, parameter = p.GetCustomAttribute<ParameterAttribute>() })
+                .Where(p => p.parameter != null)
+                .Where(p => p.parameter.Parameter.Indexed == false)
+                .OrderBy(p => p.parameter.Order)
+                .ToArray();
+
+            foreach (var nonIndexedProperty in nonIndexedProperties)
+            {
+                var searchFieldAttribute =
+                    nonIndexedProperty.property.GetCustomAttribute<SearchField>() ??
+                    new SearchField();
+
+                if (searchFieldAttribute.Ignore)
+                {
+                    continue;
+                }
+            
+                searchFieldAttribute.SourceProperty = nonIndexedProperty.property;
+                searchFieldAttribute.DataType = nonIndexedProperty.property.PropertyType;
+
+                if (string.IsNullOrEmpty(searchFieldAttribute.Name))
+                {
+                    searchFieldAttribute.Name = nonIndexedProperty.parameter.Parameter.Name;
+                }
+
+                fieldDictionary[searchFieldAttribute.Name] = searchFieldAttribute;
+                
+            }
+        }
+
+        private void LoadCustomSearchFields(Dictionary<string, SearchField> fieldDictionary)
+        {
+            var searchFields = _eventType
+                .GetProperties()
+                .Select(
+                    p => new { property = p, searchField = p.GetCustomAttribute<SearchField>()})
+                .Where(p => p.searchField != null);
+
+            foreach (var propertySearchFieldPair in searchFields)
+            {
+                var searchFieldAttribute = propertySearchFieldPair.searchField;
+
+                if (string.IsNullOrEmpty(searchFieldAttribute.Name))
+                {
+                    searchFieldAttribute.Name = propertySearchFieldPair.property.Name;
+                }
+
+                if (searchFieldAttribute.Ignore || 
+                    fieldDictionary.ContainsKey(searchFieldAttribute.Name))
+                {
+                    continue;
+                }
+
+                searchFieldAttribute.SourceProperty = propertySearchFieldPair.property;
+                searchFieldAttribute.DataType = propertySearchFieldPair.property.PropertyType;
+           
+                fieldDictionary[searchFieldAttribute.Name] = searchFieldAttribute;
+
+            }
+        }
+
+        private void LoadIndexedFields(Dictionary<string, SearchField> fieldDictionary)
+        {
+            foreach (var topicField in MapTopicsToFields())
+            {
+                if (!topicField.Ignore)
+                {
+                    fieldDictionary.Add(topicField.Name, topicField);
+                }
+            }
+        }
+
+        private SearchField[] MapTopicsToFields()
+        {
+            var topics = new TopicFilterContainer<TEvent>();
+            var fieldList = new List<SearchField>(3);
+
+            if (topics.Topic1 != TopicFilter.Empty)
+            {
+                fieldList.Add(MapTopicToField(topics.Topic1));
+            }
+            if (topics.Topic2 != TopicFilter.Empty)
+            {
+                fieldList.Add(MapTopicToField(topics.Topic2));
+            }
+            if (topics.Topic3 != TopicFilter.Empty)
+            {
+                fieldList.Add(MapTopicToField(topics.Topic3));
+            }
+
+            return fieldList.ToArray();
+        }
+
+        private SearchField MapTopicToField(TopicFilter topic)
+        {
+           var field = topic.EventDtoProperty.GetCustomAttribute<SearchField>() ??
+           new SearchField(topic.EventDtoProperty.Name)
+            {
+                IsSearchable = true,
+                IsSortable = true,
+                IsFilterable = true,
+                IsFacetable = true
+            };
+
+            field.SourceProperty = topic.EventDtoProperty;
+            field.DataType = topic.EventDtoProperty.PropertyType;
+
+            if(string.IsNullOrEmpty(field.Name))
+            {
+                field.Name = topic.EventDtoProperty.Name;
+            }
+
+            return field;
+        }
+    }
+
+    public static class SearchIndexExtensions
+    {
+        public static SearchField Field(this SearchIndex searchIndex, string name)
+        {
+            return searchIndex.Fields.FirstOrDefault(f => f.Name == name);
+        }
+
+        public static SearchField KeyField(this SearchIndex searchIndex)
+        {
+            return searchIndex.Fields.FirstOrDefault(f => f.IsKey);
+        }
+
+    }
+
+    [AttributeUsage(validOn:AttributeTargets.Class)]
+    public class Searchable : Attribute
+    {
+        public string Name { get; set; }
+        public bool AddToIndex { get; set; } = true;
+    }
+
+    [AttributeUsage(validOn:AttributeTargets.Property)]
+    public class SearchField: Attribute
+    {
+        public SearchField()
+        {
+            
+        }
+
+        public SearchField(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public Type DataType { get; set; } = typeof(System.String);
+        public bool IsKey { get; set; }
+        public bool IsSearchable { get; set; } 
+        public bool IsFilterable { get;set; }
+        public bool IsSortable { get;set; }
+        public bool IsFacetable { get; set; }
+        public bool Ignore { get; set; }
+
+        public PropertyInfo SourceProperty { get; set; }
+
+        public Func<FilterLog, object> LogValue { get; set; }
+
+        public object EventValue(IEventDTO eventDto)
+        {
+            return SourceProperty?.GetValue(eventDto);
+        }
+        
+    }
+
+
+}
