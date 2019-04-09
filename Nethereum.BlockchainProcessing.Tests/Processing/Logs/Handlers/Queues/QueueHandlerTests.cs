fc122c9bcde56891fd6e2ea9f06bbd00ea356d05
Dave Whiffin <dave.whiffin@gmail.com>
2019-04-09 18:32:18 +0100

    Queue message transformation

---
+++ Nethereum.BlockchainProcessing.Tests/Processing/Logs/Handlers/Queues/QueueHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using Nethereum.BlockchainProcessing.Processing.Logs;
+using Nethereum.BlockchainProcessing.Processing.Logs.Handling;
+using Nethereum.BlockchainProcessing.Processing.Logs.Handling.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nethereum.BlockchainProcessing.Tests.Processing.Logs.Handlers.Queues
+{
+    public class QueueHandlerTests
+    {
+        [Fact]
+        public async Task TransformsAndSendsToQueue() 
+        {
+            var subscription = new Mock<IEventSubscription>();
+            var queue = new Mock<IQueue>();
+            var handler = new QueueHandler(subscription.Object, 99, queue.Object);
+            var decodedLog = DecodedEvent.Empty();
+
+            EventLogQueueMessage actualQueueMessage = null;
+            queue
+                .Setup(q => q.AddMessageAsync(It.IsAny<object>()))
+                .Callback<object>((msg) => actualQueueMessage = msg as EventLogQueueMessage)
+                .Returns(Task.CompletedTask);
+
+            var result = await handler.HandleAsync(decodedLog);
+
+            Assert.True(result);
+            Assert.NotNull(actualQueueMessage);
+        }
+
+        [Fact]
+        public void DecodedEventToQueueMessage()
+        {
+            var log = TestData.Contracts.StandardContract.SampleTransferLog();
+            var decodedLog = log.ToDecodedEvent(TestData.Contracts.StandardContract.TransferEventAbi);
+
+            decodedLog.Transaction = new RPC.Eth.DTOs.Transaction();
+            decodedLog.State["test"] = "test";
+
+            var queueMessage = decodedLog.ToQueueMessage();
+
+            Assert.NotNull(queueMessage);
+            Assert.Equal(decodedLog.Key, queueMessage.Key);
+            Assert.Equal(decodedLog.State["test"], queueMessage.State["test"]);
+
+            Assert.Equal(decodedLog.Event.Count, queueMessage.ParameterValues.Count);
+
+            foreach(var parameter in decodedLog.Event)
+            {
+                var copy = queueMessage.ParameterValues.FirstOrDefault(p => p.Order == parameter.Parameter.Order);
+                Assert.NotNull(copy);
+                Assert.Equal(parameter.Result, copy.Value);
+                Assert.Equal(parameter.Parameter.Indexed, copy.Indexed);
+                Assert.Equal(parameter.Parameter.Name, copy.Name);
+                Assert.Equal(parameter.Parameter.ABIType.Name, copy.AbiType);
+            }
+        }
+        
+    }
+}
--- Nethereum.BlockchainProcessing/Processing/Logs/Handling/Handlers/QueueHandler.cs
+++ Nethereum.BlockchainProcessing/Processing/Logs/Handling/Handlers/QueueHandler.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using Nethereum.RPC.Eth.DTOs;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Nethereum.BlockchainProcessing.Processing.Logs.Handling.Handlers
 {
@@ -14,8 +17,57 @@ namespace Nethereum.BlockchainProcessing.Processing.Logs.Handling.Handlers
 
         public async Task<bool> HandleAsync(DecodedEvent decodedEvent)
         {
-            await Queue.AddMessageAsync(decodedEvent);
+            var msgToQueue = decodedEvent.ToQueueMessage();
+            await Queue.AddMessageAsync(msgToQueue);
             return true;
         }
+    }
+
+    public static class QueueMessageExtensions 
+    {
+        public static EventLogQueueMessage ToQueueMessage(this DecodedEvent decodedEvent)
+        {
+            var msg = new EventLogQueueMessage
+            {
+                Key = decodedEvent.Key,
+                State = decodedEvent.State,
+                Transaction = decodedEvent.Transaction,
+                Log = decodedEvent.Log,
+                ParameterValues = decodedEvent.Event.Select(p => new EventParameterValue{
+                    Order = p.Parameter.Order,
+                    AbiType = p.Parameter.ABIType.Name,
+                    Name = p.Parameter.Name,
+                    Value = p.Result,
+                    Indexed = p.Parameter.Indexed
+                }).ToList()
+            };
+
+            return msg;
+        }
+    }
+
+
+    public class EventLogQueueMessage
+    {
+        public string Key { get; set; }
+
+        public Dictionary<string, object> State { get; set; }
+
+        public Transaction Transaction { get; set; }
+
+        public List<EventParameterValue> ParameterValues { get;set;}
+
+        public FilterLog Log { get;set;}
+
+    }
+
+    public class EventParameterValue
+    {
+        public int Order { get;set;}
+        public string Name { get;set;}
+        public string AbiType { get; set;}
+        public object Value { get;set;}
+
+        public bool Indexed { get;set;}
     }
 }
