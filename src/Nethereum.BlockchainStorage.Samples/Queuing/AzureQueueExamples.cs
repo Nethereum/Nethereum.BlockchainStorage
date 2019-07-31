af661998ba1e878a39b762b3d7b254bf3dcdf38d
Dave Whiffin <dave.whiffin@gmail.com>
2019-07-31 15:29:34 +0100

    More queue examples

--- src/Nethereum.BlockchainStorage.Samples/Queuing/AzureQueueExamples.cs
+++ src/Nethereum.BlockchainStorage.Samples/Queuing/AzureQueueExamples.cs
@@ -3,6 +3,7 @@ using Nethereum.ABI.FunctionEncoding.Attributes;
 using Nethereum.BlockchainProcessing.Processor;
 using Nethereum.BlockchainProcessing.Queue;
 using Nethereum.BlockchainProcessing.Queue.Azure.Processing.Logs;
+using Nethereum.Contracts;
 using Nethereum.RPC.Eth.DTOs;
 using System;
 using System.Numerics;
@@ -38,6 +39,17 @@ namespace Nethereum.BlockchainStorage.Samples.Queuing
 
             [Parameter("uint256", "_value", 3, true)]
             public BigInteger Value { get; set; }
+        }
+
+        [Event("Approval")]
+        public class ApprovalEventDTO : IEventDTO
+        {
+            [Parameter("address", "_owner", 1, true)]
+            public virtual string Owner { get; set; }
+            [Parameter("address", "_spender", 2, true)]
+            public virtual string Spender { get; set; }
+            [Parameter("uint256", "_value", 3, false)]
+            public virtual BigInteger Value { get; set; }
         }
 
 
@@ -149,39 +161,214 @@ namespace Nethereum.BlockchainStorage.Samples.Queuing
         }
 
         [Fact]
-        public Task OneContractAnyLog()
+        public async Task OneContractAnyLog()
         {
-            throw new NotImplementedException();
+            const string QUEUE_NAME = "one-contract-any-log";
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var queue = await azureQueueFactory.GetOrCreateQueueAsync(QUEUE_NAME);
+                var eventLogProcessor = new EventLogQueueProcessor(queue);
+
+                var contractFilter = new NewFilterInput { Address = new[] { "0x109424946d5aa4425b2dc1934031d634cdad3f90" } };
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessor: eventLogProcessor, filter: contractFilter);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3146690,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3146684);
+
+                Assert.Equal(4, await queue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(QUEUE_NAME);
+            }
         }
 
         [Fact]
-        public Task OneContractOneEvent()
+        public async Task OneContractOneEvent()
         {
-            throw new NotImplementedException();
+            const string QUEUE_NAME = "one-contract-one-event";
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var queue = await azureQueueFactory.GetOrCreateQueueAsync(QUEUE_NAME);
+                var eventLogProcessor = new EventLogQueueProcessor<TransferEventDTO>(queue);
+
+                var contractFilter = new NewFilterInput { Address = new[] { "0x109424946d5aa4425b2dc1934031d634cdad3f90" } };
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessor: eventLogProcessor, filter: contractFilter);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3146690,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3146684);
+
+                Assert.Equal(1, await queue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(QUEUE_NAME);
+            }
         }
 
         [Fact]
-        public Task OneContractMultipleEvents()
+        public async Task OneContractMultipleEvents()
         {
-            throw new NotImplementedException();
+            const string ERC20_QUEUE_NAME = "one-contract-multi-event-erc20";
+            const string APPROVAL_QUEUE = "one-contract-multi-event-approval";
+
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var erc20Queue = await azureQueueFactory.GetOrCreateQueueAsync(ERC20_QUEUE_NAME);
+                var approvalQueue = await azureQueueFactory.GetOrCreateQueueAsync(APPROVAL_QUEUE);
+
+                var erc20TransferProcessor = new EventLogQueueProcessor<TransferEventDTO>(erc20Queue);
+                var approvalProcessor = new EventLogQueueProcessor<ApprovalEventDTO>(approvalQueue);
+                var logProcessors = new ProcessorHandler<FilterLog>[] { erc20TransferProcessor, approvalProcessor };
+
+                var contractFilter = new NewFilterInput
+                {
+                    Address = new[] { "0x9EDCb9A9c4d34b5d6A082c86cb4f117A1394F831" }
+                };
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessors: logProcessors, filter: contractFilter);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3621716,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3621715);
+
+                Assert.Equal(2, await erc20Queue.GetApproxMessageCountAsync());
+                Assert.Equal(1, await approvalQueue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(ERC20_QUEUE_NAME);
+                await azureQueueFactory.DeleteQueueAsync(APPROVAL_QUEUE);
+            }
         }
 
         [Fact]
-        public Task ManyContractsAnyLog()
+        public async Task ManyContractsAnyLog()
         {
-            throw new NotImplementedException();
+            const string QUEUE_NAME = "many-contract-any-log";
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var queue = await azureQueueFactory.GetOrCreateQueueAsync(QUEUE_NAME);
+                var eventLogProcessor = new EventLogQueueProcessor(queue);
+
+                var contractAddresses = new[] { "0x109424946d5aa4425b2dc1934031d634cdad3f90", "0x16c45b25c4817bdedfce770f795790795c9505a6" };
+
+                var contractFilter = new NewFilterInput { Address = contractAddresses };
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessor: eventLogProcessor, filter: contractFilter);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3146690,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3146684);
+
+                Assert.Equal(8, await queue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(QUEUE_NAME);
+            }
         }
 
         [Fact]
-        public Task ManyContractsOneEvent()
+        public async Task ManyContractsOneEvent()
         {
-            throw new NotImplementedException();
+            const string QUEUE_NAME = "many-contracts-one-event";
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var queue = await azureQueueFactory.GetOrCreateQueueAsync(QUEUE_NAME);
+                var eventLogProcessor = new EventLogQueueProcessor<TransferEventDTO>(queue);
+
+                var contractAddresses = new[] { "0x109424946d5aa4425b2dc1934031d634cdad3f90", "0x16c45b25c4817bdedfce770f795790795c9505a6" };
+
+                var contractFilter = new NewFilterInput { Address = contractAddresses };
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessor: eventLogProcessor, filter: contractFilter);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3146690,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3146684);
+
+                Assert.Equal(5, await queue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(QUEUE_NAME);
+            }
         }
 
         [Fact]
-        public Task ManyContractsMultipleEvents()
+        public async Task ManyContractsMultipleEvents()
         {
-            throw new NotImplementedException();
+            const string ERC20_QUEUE_NAME = "many-contract-multi-event-erc20";
+            const string APPROVAL_QUEUE_NAME = "many-contract-multi-event-approval";
+
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var erc20Queue = await azureQueueFactory.GetOrCreateQueueAsync(ERC20_QUEUE_NAME);
+                var approvalQueue = await azureQueueFactory.GetOrCreateQueueAsync(APPROVAL_QUEUE_NAME);
+
+                var erc20TransferProcessor = new EventLogQueueProcessor<TransferEventDTO>(erc20Queue);
+                var approvalProcessor = new EventLogQueueProcessor<ApprovalEventDTO>(approvalQueue);
+                var logProcessors = new ProcessorHandler<FilterLog>[] { erc20TransferProcessor, approvalProcessor };
+
+                var contractAddresses = new[] { "0x9EDCb9A9c4d34b5d6A082c86cb4f117A1394F831", "0xafbfefa496ae205cf4e002dee11517e6d6da3ef6" };
+
+                var contractFilter = new NewFilterInput { Address = contractAddresses };
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessors, filter: contractFilter);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3621716,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3621715);
+
+                Assert.Equal(2, await erc20Queue.GetApproxMessageCountAsync());
+                Assert.Equal(1, await approvalQueue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(ERC20_QUEUE_NAME);
+                await azureQueueFactory.DeleteQueueAsync(APPROVAL_QUEUE_NAME);
+            }
         }
 
         public class MessageToQueue
@@ -193,15 +380,25 @@ namespace Nethereum.BlockchainStorage.Samples.Queuing
                 LogIndex = log.LogIndex.ToString();
             }
 
+            public MessageToQueue(EventLog<TransferEventDTO> transferEventLog)
+            {
+                BlockNumber = transferEventLog.Log.BlockNumber.Value.ToString();
+                TransactionHash = transferEventLog.Log.TransactionHash;
+                LogIndex = transferEventLog.Log.LogIndex.ToString();
+                EventName = transferEventLog.Event.GetEventABI().Name;
+            }
+
+            public string EventName { get; set;}
+
             public string BlockNumber { get;set;}
             public string TransactionHash { get;set;}
             public string LogIndex { get;set;}
         }
 
         [Fact]
-        public async Task WithMapping()
+        public async Task MappingALogToACustomQueueMessage()
         {
-            const string QUEUE_NAME = "with-mapping";
+            const string QUEUE_NAME = "mapping-from-log";
             var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
             try
             {
@@ -222,6 +419,37 @@ namespace Nethereum.BlockchainStorage.Samples.Queuing
                     startAtBlockNumberIfNotProcessed: 3146684);
 
                 Assert.Equal(65, await queue.GetApproxMessageCountAsync());
+            }
+            finally
+            {
+                await azureQueueFactory.DeleteQueueAsync(QUEUE_NAME);
+            }
+        }
+
+        [Fact]
+        public async Task MappingAnEventLogToACustomQueueMessage()
+        {
+            const string QUEUE_NAME = "mapping-from-event-log";
+            var azureQueueFactory = new AzureStorageQueueFactory(_azureConnectionString);
+            try
+            {
+                var queue = await azureQueueFactory.GetOrCreateQueueAsync(QUEUE_NAME);
+
+                var eventLogProcessor = new EventLogQueueProcessor<TransferEventDTO>(
+                    destinationQueue: queue, mapper: (log) => new MessageToQueue(log));
+
+                var logProcessor = _web3.Processing.Logs.CreateProcessor(logProcessor: eventLogProcessor);
+
+                //if we need to stop the processor mid execution - call cancel on the token
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                //crawl the required block range
+                await logProcessor.ExecuteAsync(
+                    toBlockNumber: 3146690,
+                    cancellationToken: cancellationTokenSource.Token,
+                    startAtBlockNumberIfNotProcessed: 3146684);
+
+                Assert.Equal(13, await queue.GetApproxMessageCountAsync());
             }
             finally
             {
