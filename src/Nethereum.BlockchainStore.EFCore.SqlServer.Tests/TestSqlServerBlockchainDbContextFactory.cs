51d1bbe493c17d49ab976707c52b6aa73981620e
Dave Whiffin <dave.whiffin@gmail.com>
2020-02-12 10:17:40 +0000

    Fixing test connection strings and update .net core from 2.1 to 3.1

--- src/Nethereum.BlockchainStore.EFCore.SqlServer.Tests/TestSqlServerBlockchainDbContextFactory.cs
+++ src/Nethereum.BlockchainStore.EFCore.SqlServer.Tests/TestSqlServerBlockchainDbContextFactory.cs
@@ -6,7 +6,7 @@ namespace Nethereum.BlockchainStore.EFCore.Tests.SqlServer
     public class TestSqlServerBlockchainDbContextFactory : SqlServerCoreBlockchainDbContextFactory
     {
         public const string ConnectionString =
-            "Data Source=localhost\\SQLEXPRESS01;Database=BlockchainStorage;Integrated Security=False;User ID=blockchaindbo1;Password=bALLfMA1wBlJCzSGZhkO;Connect Timeout=60;";
+            "Data Source=localhost\\SQLDEV;Database=BlockchainStorage;Integrated Security=False;User ID=blockchaindbo1;Password=bALLfMA1wBlJCzSGZhkO;Connect Timeout=60;";
 
         public TestSqlServerBlockchainDbContextFactory() : base(ConnectionString, DbSchemaNames.dbo)
         {
