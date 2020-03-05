677162bbb0a5d5d4d68b8c0dfa78efb2ff4e2de4
Dave Whiffin <dave.whiffin@gmail.com>
2020-03-05 12:33:10 +0000

    Provide specific error for SQL Server Data Truncation error
    For existing block storage users who may have a BlockProgress table with the old schema and a shorter LastBlockProcessed column length. Catch EF and EFCore exceptions for the UpsertBlockProgressAsync method when data exceeds the max length.  Provide an exception with a message to indicate that the LastBlockProcessed column may not be large enough and states what the column length should be.  Includes the root exception in the inner exception.

--- src/Nethereum.BlockchainStore.EF/Repositories/BlockProgressRepository.cs
+++ src/Nethereum.BlockchainStore.EF/Repositories/BlockProgressRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Nethereum.BlockchainProcessing.BlockStorage.Entities;
 using Nethereum.BlockchainProcessing.BlockStorage.Entities.Mapping;
 using Nethereum.BlockchainProcessing.ProgressRepositories;
+using System;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Numerics;
 using System.Threading.Tasks;
 
@@ -24,12 +26,22 @@ namespace Nethereum.BlockchainStore.EF.Repositories
 
         public async Task UpsertProgressAsync(BigInteger blockNumber)
         {
-            using (var context = _contextFactory.CreateContext())
+            try
             {
-                var blockEntity = blockNumber.MapToStorageEntityForUpsert();
-                blockEntity.LastBlockProcessed = blockEntity.LastBlockProcessed.PadLeft(ColumnLengths.BigIntegerLength, '0');
-                context.BlockProgress.Add(blockEntity);
-                await context.SaveChangesAsync().ConfigureAwait(false);
+                using (var context = _contextFactory.CreateContext())
+                {
+                    var blockEntity = blockNumber.MapToStorageEntityForUpsert();
+                    blockEntity.LastBlockProcessed = blockEntity.LastBlockProcessed.PadLeft(ColumnLengths.BigIntegerLength, '0');
+                    context.BlockProgress.Add(blockEntity);
+                    await context.SaveChangesAsync().ConfigureAwait(false);
+                }
+            }
+            catch(DbUpdateException ex) 
+            when (ex.InnerException?.InnerException.Message.Contains("String or binary data would be truncated") ?? false)
+            {
+                throw new DbUpdateException(
+                    $"{nameof(BlockProgressRepository)} Data Truncation Rrror. Ensure that the LastBlockProcessed column length is {ColumnLengths.BigIntegerLength}."
+                    , ex);
             }
         }
     }
--- src/Nethereum.BlockchainStore.EFCore/Repositories/BlockProgressRepository.cs
+++ src/Nethereum.BlockchainStore.EFCore/Repositories/BlockProgressRepository.cs
@@ -4,6 +4,7 @@ using Nethereum.BlockchainProcessing.BlockStorage.Entities;
 using Nethereum.BlockchainProcessing.BlockStorage.Entities.Mapping;
 using System.Numerics;
 using System.Threading.Tasks;
+using System;
 
 namespace Nethereum.BlockchainStore.EFCore.Repositories
 {
@@ -24,15 +25,21 @@ namespace Nethereum.BlockchainStore.EFCore.Repositories
 
         public async Task UpsertProgressAsync(BigInteger blockNumber)
         {
-            using (var context = _contextFactory.CreateContext())
+            try
             {
-                var blockRange = blockNumber.MapToStorageEntityForUpsert<BlockProgress>();
-
-                blockRange.LastBlockProcessed = blockNumber.ToString().PadLeft(ColumnLengths.BigIntegerLength, '0');
-
-                context.BlockProgress.Add(blockRange);
-
-                await context.SaveChangesAsync().ConfigureAwait(false);
+                using (var context = _contextFactory.CreateContext())
+                {
+                    var blockRange = blockNumber.MapToStorageEntityForUpsert<BlockProgress>();
+                    blockRange.LastBlockProcessed = blockNumber.ToString().PadLeft(ColumnLengths.BigIntegerLength, '0');
+                    context.BlockProgress.Add(blockRange);
+                    await context.SaveChangesAsync().ConfigureAwait(false);
+                }
+            }
+            catch (DbUpdateException ex) when (ex.InnerException?.Message.Contains("String or binary data would be truncated") ?? false)
+            {
+                throw new DbUpdateException(
+                    $"{nameof(BlockProgressRepository)} Data Truncation Rrror. Ensure that the LastBlockProcessed column length is {ColumnLengths.BigIntegerLength}."
+                    , ex);
             }
         }
     }
